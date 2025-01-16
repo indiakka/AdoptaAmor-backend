f5c2a1daa99aec01d187eb906946e5ffd360e2ac
indiakka <78439130+indiakka@user.noreply.github.com>
2025-01-16 12:18:20 +0100

    fixed cloudinary sets

--- src/main/java/com/adoptaamor/adoptaamor/services/FileStorageService.java
+++ src/main/java/com/adoptaamor/adoptaamor/services/FileStorageService.java
@@ -4,32 +4,28 @@ import java.io.IOException;
 import java.util.Map;
 import java.util.UUID;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @Service
 public class FileStorageService {
 
     private final Cloudinary cloudinary;
 
-    public FileStorageService(
-            @Value("${cloudinary.cloud_name}") String cloudName,
-            @Value("${cloudinary.api_key}") String apiKey,
-            @Value("${cloudinary.api_secret}") String apiSecret) {
-        cloudinary = new Cloudinary(ObjectUtils.asMap(
-                "cloud_name", cloudName,
-                "api_key", apiKey,
-                "api_secret", apiSecret));
-
-        System.out.println("name: " + cloudName);
-        System.out.println("key: " + apiKey);
-        System.out.println("secret: " + apiSecret);
+    public FileStorageService() {
+        Dotenv dotenv = Dotenv.load();
+        this.cloudinary = new Cloudinary(ObjectUtils.asMap(
+                "cloud_name", dotenv.get("CLOUDINARY_CLOUD_NAME"),
+                "api_key", dotenv.get("CLOUDINARY_API_KEY"),
+                "api_secret", dotenv.get("CLOUDINARY_API_SECRET")));
     }
 
+    @SuppressWarnings("unchecked")
     public String storeFile(MultipartFile file) {
 
         if (file == null || file.isEmpty()) {
@@ -37,18 +33,15 @@ public class FileStorageService {
         }
 
         try {
-
-            String fileName = UUID.randomUUID().toString().replace("-", "");
-            fileName = fileName.replaceAll("[^a-zA-Z0-9]", "");
-            fileName = fileName + "_" + UUID.randomUUID().toString().replace("-", "").substring(0, 15);
+            String fileName = UUID.randomUUID().toString().replace("-", "")
+                    + "_" + UUID.randomUUID().toString().replace("-", "").substring(0, 15);
 
             Map<String, Object> uploadResult = cloudinary.uploader().upload(file.getBytes(),
                     ObjectUtils.asMap(
                             "resource_type", "auto",
                             "public_id", fileName));
 
-            String publicUrl = uploadResult.get("secure_url").toString();
-            return publicUrl;
+            return uploadResult.get("secure_url").toString();
         } catch (IOException e) {
             throw new RuntimeException("Error al subir el archivo a Cloudinary", e);
         }
